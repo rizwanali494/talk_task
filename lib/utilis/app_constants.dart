cc1c15e7904d1efbcc0370cfc895b8226ec3eaf1
dumdum33 <dummyjd448@gmail.com>
2024-11-06 22:45:50 +0500

    demo screens done

--- lib/utilis/app_constants.dart
+++ lib/utilis/app_constants.dart
@@ -72,6 +72,25 @@ class AppConstants {
   static const String privacyOnlyAggregated =
       'Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Service Provider may share your information with third parties in the ways that are described in this privacy statement.\nPlease note that the Application utilizes third-party services that have their own Privacy Policy about handling data. Below are the links to the Privacy Policy of the third-party service providers used by the Application:';
 
+
+  static const String optOutRights = "Opt-Out Rights";
+  static const String optOutRightsDescription = "You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.";
+
+  static const String dataRetention = "Data Retention Policy";
+  static const String dataRetentionDescription = "The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at TaskTalkApp@gmail.com and they will respond in a reasonable time";
+
+  static const String children = "Children";
+  static const String childrenDescription = "The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13. The Application does not address anyone under the age of 13. The Service Provider does not knowingly collect personally identifiable information from children under 13 years of age. In the case the Service Provider discover that a child under 13 has provided personal information, the Service Provider will immediately delete this from their servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact the Service Provider (TaskTalkApp@gmail.com) so that they will be able to take the necessary actions.";
+
+  static const String security = "Security";
+  static const String securityDescription = "The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect information the Service Provider processes and maintains.";
+  static const String changes = "Changes";
+  static const String changesDescription = "This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to the Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.This privacy policy is effective as of 2024-06-29";
+  static const String yourConsent = "Your Consent";
+  static const String yourConsentDescription = "By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.";
+
+
+
 //terms and conditions
   static const String termsAndConditions = "Terms & Conditions";
 
--- lib/view/screens/setting_screen/privacy_screen.dart
+++ lib/view/screens/setting_screen/privacy_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:talk_task/utilis/app_constants.dart';
 import '../../../utilis/app_colors.dart';
+import '../../../utilis/app_text_styles.dart';
 import '../../common_widgets/custom_app_bars.dart';
 import '../../common_widgets/custom_text.dart';
 
@@ -70,9 +71,69 @@ class PrivacyScreen extends StatelessWidget{
                             AppConstants.privacyOnlyAggregated,
                         fontSize: 16.sp,
                         fontWeight: FontWeight.w300),
+
+                    //Opt Out Rights
+                    _simpleHeadingValue(title: AppConstants.optOutRights, description:  AppConstants.optOutRightsDescription),
+
+                    //Data Retention
+                    _simpleHeadingValue(title: AppConstants.dataRetention, description:  AppConstants.dataRetentionDescription),
+
+                    //Children
+                    _simpleHeadingValue(title: AppConstants.children, description:  AppConstants.childrenDescription),
+
+
+                    //Security
+                    _simpleHeadingValue(title: AppConstants.security, description:  AppConstants.securityDescription),
+
+                    //Changes
+                    _simpleHeadingValue(title: AppConstants.changes, description:  AppConstants.changesDescription),
+
+                    //Your Consent
+                    _simpleHeadingValue(title: AppConstants.yourConsent, description:  AppConstants.yourConsentDescription),
+
+
+                    //Contact Us
+                    _simpleHeadingValue(title:AppConstants.contactUs , description:  AppConstants.contactUsFirst,),
+
+                    SizedBox(height: 3.h),
+                    Text( AppConstants.contactUsEmail,style: AppTextStyles.poppins(
+                        shouldUnderline: true,
+                        color: AppColors.black, fontSize: 16.sp,weight: FontWeight.w300),)
+                    ,
+                    SizedBox(height: 3.h),
+
+                    CustomText(
+                        text:
+                        AppConstants.contactUsSecond,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w300),
+                    SizedBox(height: 3.h),
                   ],
                 ),
               ),
             )));
   }
+
+
+
+  Widget _simpleHeadingValue({required String title,required String description}){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+      SizedBox(height: 3.h),
+      CustomText(
+        text:
+        title,
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w700,
+        color: AppColors.primary,),
+      SizedBox(height: 3.h),
+      CustomText(
+          text:
+          description,
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w300),
+    ],);
+  }
 }
+
