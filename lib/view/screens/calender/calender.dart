8b4a3c48f9bfef336d408bf66cba4ce4f08c4d81
jawad4353 <ja883526@gmail.com>
2024-11-14 17:19:41 +0500

    working on time dialogue

--- lib/view/screens/calender/calender.dart
+++ lib/view/screens/calender/calender.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:talk_task/view/screens/dialogues/pick_date_dialogue.dart';
 import '../../../utilis/app_colors.dart';
 import '../../../utilis/app_constants.dart';
 import '../../../view_model/events_listner_provider.dart';
 import '../../../view_model/provider_list.dart';
+import '../../common_widgets/custom_app_bars.dart';
 import '../../common_widgets/custom_cards.dart';
 import '../../common_widgets/custom_text.dart';
+import 'date_calender.dart';
 
 
 
@@ -34,14 +35,17 @@ class _CalenderScreenState extends State<CalenderScreen> {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: AppColors.whiteFFFFF,
+      appBar: AppBars.settingsAppBar(context: context, title: AppConstants.tasks),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const PickDateDialogue(),
+            SizedBox(height: 8.h,),
+            const CalenderDateUI(),
           Padding(
             padding:  EdgeInsets.only(left: 16.w),
-            child: CustomText(text: AppConstants.upcomingEvents,fontWeight: FontWeight.w700,fontSize: 20.sp,color: AppColors.blueDark002055,),
+            child: CustomText(text: AppConstants.upcomingEvents,
+              fontWeight: FontWeight.w700,fontSize: 20.sp,color: AppColors.blueDark002055,),
           ),
           SizedBox(height: 5.h,),
           _displayEvents()
---
+++ lib/view/screens/calender/date_calender.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:talk_task/utilis/app_constants.dart';
+import 'package:talk_task/utilis/app_text_styles.dart';
+import '../../../utilis/app_colors.dart';
+import '../../../utilis/app_images.dart';
+import '../../../view_model/date_picker_provider.dart';
+import '../../common_widgets/custom_buttons.dart';
+import '../../common_widgets/custom_text.dart';
+
+class CalenderDateUI extends StatefulWidget {
+  const CalenderDateUI({super.key}) ;
+
+  @override
+  State<CalenderDateUI> createState() => _CalenderDateUI();
+}
+class _CalenderDateUI extends State<CalenderDateUI> {
+  DateTime? _selectedDate;
+  DateTime _currentDate = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<DatePickerProvider>(context, listen: false).selectedDate = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime today = DateTime.now().subtract(const Duration(days: 1));
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5.w),
+      color: AppColors.whiteFFFFF,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Container(
+            clipBehavior: Clip.antiAlias,
+            height: 360.h,
+            margin: EdgeInsets.symmetric(horizontal: 7.w),
+            decoration: BoxDecoration(
+              color: AppColors.whiteFFFFF,
+              boxShadow: [
+                BoxShadow(
+                    color: AppColors.grey787878.withOpacity(0.4),
+                    spreadRadius: 0.1,
+                    blurRadius: 0.3)
+              ],
+              borderRadius: BorderRadius.circular(14.r),
+            ),
+            child: Column(
+              children: [
+                // Custom Header with navigation buttons
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Image.asset(AppImages.iconBackArrow, color: AppColors.black, height: 25.h),
+                        onPressed: () {
+                          setState(() {
+                            _currentDate = DateTime(_currentDate.year, _currentDate.month - 1, 1);
+                          });
+                        },
+                      ),
+                      CustomText(
+                        text: DateFormat('MMMM yyyy').format(_currentDate),
+                        fontSize: 18.sp,
+                        color: AppColors.primary,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      IconButton(
+                        icon: Image.asset(AppImages.iconForward, color: AppColors.black, height: 25.h),
+                        onPressed: () {
+                          setState(() {
+                            _currentDate = DateTime(_currentDate.year, _currentDate.month + 1, 1);
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                // Calendar view
+                Expanded(
+                  child: SfCalendar(
+                    selectionDecoration: const BoxDecoration(),
+                    initialDisplayDate: _currentDate,
+                    view: CalendarView.month,
+                    cellBorderColor: AppColors.transparent,
+                    firstDayOfWeek: 1,
+                    showNavigationArrow: false, // Disable default navigation arrows
+                    onSelectionChanged: (calenderDetails) {
+                      final DateTime selectedDate = calenderDetails.date!;
+                      if (selectedDate.isBefore(today)) {
+                        return; // Ignore selection if the date is before today
+                      }
+
+                      setState(() {
+                        _selectedDate = selectedDate;
+                      });
+                      context.read<DatePickerProvider>().selectDate(selectedDate);
+                    },
+
+                    headerHeight: 0, // Hide default header
+                    monthCellBuilder: (BuildContext context, MonthCellDetails details) {
+                      final DateTime date = details.date;
+                      bool isSelected = _selectedDate != null && _selectedDate!.isAtSameMomentAs(date);
+                      bool isBeforeToday = date.isBefore(today);
+                      Color textColor = isBeforeToday
+                          ? AppColors.black.withOpacity(0.5)
+                          : (isSelected
+                          ? AppColors.whiteFFFFF // Selected date text color
+                          : AppColors.black); // Default text color for future dates
+
+                      // Cell background color remains unchanged for past dates
+                      Color cellColor = isSelected
+                          ? AppColors.primary // Background color for selected date
+                          : AppColors.transparent; // No background color for other dates
+
+                      return ClipOval(
+
+                        child: Container(
+                          decoration: BoxDecoration(shape: BoxShape.circle,
+                            color: cellColor,
+                          ),
+                          // Only apply background color here
+                          child: Center(
+                            child: CustomText(
+                              text: date.day.toString(),
+                              color: textColor, // Apply text color
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13.sp,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 20.h),
+        ],
+      ),
+    );
+  }
+}
